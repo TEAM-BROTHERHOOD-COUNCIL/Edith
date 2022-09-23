39631293f0c8a88a69932f160b681aaee57f7a12
BROTHERHOOD OP <114099824+TEAM-BROTHERHOOD-COUNCIL@users.noreply.github.com>
2022-09-23 12:03:54 +0530

    Add files via upload

---
+++ TBH/helper.py
@@ -0,0 +1,6 @@
+HELP_IMG = ["https://te.legra.ph/file/aa3f43c174f03b422aea8.jpg"]
+
+START_IMG = ["https://te.legra.ph/file/dbd33d6f299878bc3c828.jpg"]
+
+
+PHOTO = ["https://te.legra.ph/file/aa3f43c174f03b422aea8.jpg"]
