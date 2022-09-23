bccacfcbf2aae75b074c0984dfcb0b8c141538d5
BROTHERHOOD OP <114099824+TEAM-BROTHERHOOD-COUNCIL@users.noreply.github.com>
2022-09-23 23:59:42 +0530

    Update carbon.py

--- Edith/utils/carbon.py
+++ Edith/utils/carbon.py
@@ -35,5 +35,5 @@ async def make_carbon(code):
     url = "https://carbonara.vercel.app/api/cook"
     async with aiohttpsession.post(url, json={"code": code}) as resp:
         image = BytesIO(await resp.read())
-    image.name = "Exon_Carbon.png"
+    image.name = "Edith_Carbon.png"
     return image
