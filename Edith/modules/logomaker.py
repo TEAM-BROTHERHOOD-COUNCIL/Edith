d0e61b1fb2789eb138e52219d7e219ed414ce359
BROTHERHOOD OP <114099824+TEAM-BROTHERHOOD-COUNCIL@users.noreply.github.com>
2022-09-25 00:42:40 +0530

    Update logomaker.py

--- Edith/modules/logomaker.py
+++ Edith/modules/logomaker.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 Aʙɪsʜɴᴏɪ
+Copyright (c) 2022 BROTHERHOOD COUNCIL
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+# ""DEAR PRO PEOPLE,  IF YOU REMOVE & CHANGE THIS LINE KINDLY INFORM US AT @TBH_COUNCIL_SUPPORT
+# TG :- @THE_BROTHERHOOD_COUNCIL
+#     MY ALL BOTS :- BROTHERHOOD_BOTS
+#     GITHUB :- THE-BROTHERHOOD-COUNCIL ""
 
 import glob
 import io
@@ -30,8 +34,8 @@ import random
 import requests
 from PIL import Image, ImageDraw, ImageFont
 
-from Exon import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT, telethn
-from Exon.events import register
+from Edith import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT, telethn
+from Edith.events import register
 
 LOGO_LINKS = [
     "https://telegra.ph/file/d1838efdafce9fe611d0c.jpg",
@@ -290,7 +294,7 @@ async def lego(event):
 
         image_widthz, image_heightz = img.size
 
-        fnt = glob.glob("./Exon/utils/Logo/*")
+        fnt = glob.glob("./Edith/utils/Logo/*")
 
         randf = random.choice(fnt)
 
