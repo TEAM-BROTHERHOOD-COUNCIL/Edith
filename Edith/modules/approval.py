ddf39c37a05f5ae38b7fc71fe932b479c56f6dcf
BROTHERHOOD OP <114099824+TEAM-BROTHERHOOD-COUNCIL@users.noreply.github.com>
2022-09-24 23:36:32 +0530

    Update approval.py

--- Edith/modules/approval.py
+++ Edith/modules/approval.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 Aʙɪsʜɴᴏɪ
+Copyright (c) 2022 BROTHERHOOD COUNCIL
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+# ""DEAR PRO PEOPLE,  IF YOU REMOVE & CHANGE THIS LINE KINDLY INFORM US AT @TBH_COUNCIL_SUPPORT
+# TG :- @THE_BROTHERHOOD_COUNCIL
+#     MY ALL BOTS :- BROTHERHOOD_BOTS
+#     GITHUB :- THE-BROTHERHOOD-COUNCIL ""
 
 import html
 
@@ -29,12 +33,12 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 from telegram.utils.helpers import mention_html
 
-import Exon.modules.sql.approve_sql as sql
-from Exon import DRAGONS, dispatcher
-from Exon.modules.disable import DisableAbleCommandHandler
-from Exon.modules.helper_funcs.chat_status import user_admin
-from Exon.modules.helper_funcs.extraction import extract_user
-from Exon.modules.log_channel import loggable
+import Edith.modules.sql.approve_sql as sql
+from Edith import DRAGONS, dispatcher
+from Edith.modules.disable import DisableAbleCommandHandler
+from Edith.modules.helper_funcs.chat_status import user_admin
+from Edith.modules.helper_funcs.extraction import extract_user
+from Edith.modules.log_channel import loggable
 
 
 @loggable
