90fa0cac9a23deaef0ce0c646a5bdd88db17b8c6
BROTHERHOOD OP <114099824+TEAM-BROTHERHOOD-COUNCIL@users.noreply.github.com>
2022-09-24 23:54:54 +0530

    Update books.py

--- Edith/modules/books.py
+++ Edith/modules/books.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 Aʙɪsʜɴᴏɪ
+Copyright (c) 2022 BROTHERHOOD COUNCIL
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+# ""DEAR PRO PEOPLE,  IF YOU REMOVE & CHANGE THIS LINE KINDLY INFORM US AT @TBH_COUNCIL_SUPPORT
+# TG :- @THE_BROTHERHOOD_COUNCIL
+#     MY ALL BOTS :- BROTHERHOOD_BOTS
+#     GITHUB :- THE-BROTHERHOOD-COUNCIL ""
 
 import os
 import re
@@ -29,7 +33,7 @@ import requests
 from bs4 import BeautifulSoup
 from telethon import events
 
-from Exon import BOT_USERNAME, SUPPORT_CHAT, telethn
+from Edith import BOT_USERNAME, SUPPORT_CHAT, telethn
 
 
 @telethn.on(events.NewMessage(pattern="^/book (.*)"))
@@ -76,7 +80,7 @@ async def _(event):
         await telethn.send_file(
             event.chat_id,
             "book.txt",
-            caption=f"**BOOKS GATHERED SUCCESSFULLY!\n\nBY @ABISHNOIMF **",
+            caption=f"**BOOKS GATHERED SUCCESSFULLY!\n\nBY @TEAM_BLACK_SUPPORT **",
         )
         os.remove("book.txt")
         await KkK.delete()
