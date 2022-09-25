43135c4e560fd1a14ae513539daf39978a7dc01b
BROTHERHOOD OP <114099824+TEAM-BROTHERHOOD-COUNCIL@users.noreply.github.com>
2022-09-25 22:18:18 +0530

    Update ytmusic.py

--- Edith/modules/ytmusic.py
+++ Edith/modules/ytmusic.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 Aʙɪsʜɴᴏɪ
+Copyright (c) 2022 BROTHERHOOD COUNCIL
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+# ""DEAR PRO PEOPLE,  IF YOU REMOVE & CHANGE THIS LINE KINDLY INFORM US AT @TBH_COUNCIL_SUPPORT
+# TG :- @THE_BROTHERHOOD_COUNCIL
+#     MY ALL BOTS :- BROTHERHOOD_BOTS
+#     GITHUB :- THE-BROTHERHOOD-COUNCIL ""
 
 import datetime
 import os
@@ -32,10 +36,10 @@ from pyrogram import filters
 from pytube import YouTube
 from requests import get
 
-from Exon import aiohttpsession as session
-from Exon import arq, pgram
-from Exon.core.decorators.errors import capture_err
-from Exon.utils.pastebin import paste
+from Edith import aiohttpsession as session
+from Edith import arq, pgram
+from Edith.core.decorators.errors import capture_err
+from Edith.utils.pastebin import paste
 
 is_downloading = False
 
