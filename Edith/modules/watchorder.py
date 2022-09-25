7675dad0f98f29577a79f05a372109a4d45f1923
BROTHERHOOD OP <114099824+TEAM-BROTHERHOOD-COUNCIL@users.noreply.github.com>
2022-09-25 22:14:34 +0530

    Update watchorder.py

--- Edith/modules/watchorder.py
+++ Edith/modules/watchorder.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 A𝚋𝚒𝚜𝚑𝚗𝚘𝚒
+Copyright (c) 2022 BROTHERHOOD COUNCIL
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,12 +21,16 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+# ""DEAR PRO PEOPLE,  IF YOU REMOVE & CHANGE THIS LINE KINDLY INFORM US AT @TBH_COUNCIL_SUPPORT
+# TG :- @THE_BROTHERHOOD_COUNCIL
+#     MY ALL BOTS :- BROTHERHOOD_BOTS
+#     GITHUB :- THE-BROTHERHOOD-COUNCIL ""
 
 import requests
 from bs4 import BeautifulSoup
 from pyrogram import filters
 
-from Exon import pgram
+from Edith import pgram
 
 
 @pgram.on_message(filters.command("watchorder"))
