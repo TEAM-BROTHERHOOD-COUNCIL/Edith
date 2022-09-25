bcf9e87982886668f586608f83ce884f9d498b28
BROTHERHOOD OP <114099824+TEAM-BROTHERHOOD-COUNCIL@users.noreply.github.com>
2022-09-25 21:45:59 +0530

    Update reporting.py

--- Edith/modules/reporting.py
+++ Edith/modules/reporting.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 Aʙɪsʜɴᴏɪ
+Copyright (c) 2022 BROTHERHOOD COUNCIL
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+# ""DEAR PRO PEOPLE,  IF YOU REMOVE & CHANGE THIS LINE KINDLY INFORM US AT @TBH_COUNCIL_SUPPORT
+# TG :- @THE_BROTHERHOOD_COUNCIL
+#     MY ALL BOTS :- BROTHERHOOD_BOTS
+#     GITHUB :- THE-BROTHERHOOD-COUNCIL ""
 
 import html
 
@@ -35,10 +39,10 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html
 
-from Exon import DRAGONS, LOGGER, TIGERS, WOLVES, dispatcher
-from Exon.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from Exon.modules.log_channel import loggable
-from Exon.modules.sql import reporting_sql as sql
+from Edith import DRAGONS, LOGGER, TIGERS, WOLVES, dispatcher
+from Edith.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from Edith.modules.log_channel import loggable
+from Edith.modules.sql import reporting_sql as sql
 
 REPORT_GROUP = 12
 REPORT_IMMUNE_USERS = DRAGONS + TIGERS + WOLVES
