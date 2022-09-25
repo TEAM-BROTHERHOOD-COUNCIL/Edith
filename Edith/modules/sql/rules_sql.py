a5ef1269d897b164da24c035ea79358da08e10d0
BROTHERHOOD OP <114099824+TEAM-BROTHERHOOD-COUNCIL@users.noreply.github.com>
2022-09-25 23:32:32 +0530

    Update rules_sql.py

--- Edith/modules/sql/rules_sql.py
+++ Edith/modules/sql/rules_sql.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 Aʙɪsʜɴᴏɪ
+Copyright (c) 2022 BROTHERHOOD COUNCIL
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,12 +21,16 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+# ""DEAR PRO PEOPLE,  IF YOU REMOVE & CHANGE THIS LINE KINDLY INFORM US AT @TBH_COUNCIL_SUPPORT
+# TG :- @THE_BROTHERHOOD_COUNCIL
+#     MY ALL BOTS :- BROTHERHOOD_BOTS
+#     GITHUB :- THE-BROTHERHOOD-COUNCIL ""
 
 import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
-from Exon.modules.sql import BASE, SESSION
+from Edith.modules.sql import BASE, SESSION
 
 
 class Rules(BASE):
